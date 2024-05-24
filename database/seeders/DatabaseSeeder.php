fe33f73c2e1d78c77db8ca882b804192ba44cb06
Jessen Chayadi <113822329+Ceee17@users.noreply.github.com>
2024-05-24 15:07:43 +0700

    add AdminLte template

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,9 +14,11 @@ class DatabaseSeeder extends Seeder
     {
         // \App\Models\User::factory(10)->create();
 
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        \App\Models\User::factory()->create([
+            'name' => 'test',
+            'email' => 'test@example.com',
+            'email_verified_at' => now(),
+            'password' => 'test123',
+        ]);
     }
 }
