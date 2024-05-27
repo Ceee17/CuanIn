050d6f95c801aebf1744413d58a7950524d48994
Jessen Chayadi <113822329+Ceee17@users.noreply.github.com>
2024-05-27 08:57:02 +0700

    add migration database

---
+++ database/migrations/2024_05_26_093240_create_purchases_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('purchases', function (Blueprint $table) {
+            $table->increments('purchase_id');
+            $table->integer('supplier_id');
+            $table->integer('total_item');
+            $table->integer('total_price');
+            $table->tinyInteger('discount')->default(0);
+            $table->integer('payment');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('purchases');
+    }
+};
