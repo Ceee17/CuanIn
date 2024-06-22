614b52115360d89963b401b709d1dbf7bc97e8ed
finniaaa <144453150+finniaaa@users.noreply.github.com>
2024-06-22 20:05:20 +0700

    membuat crud membership, dan menambah fitur cetak member tapi masih error

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,12 +13,9 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         // \App\Models\User::factory(10)->create();
-
-        \App\Models\User::factory()->create([
-            'name' => 'test',
-            'email' => 'test@example.com',
-            'email_verified_at' => now(),
-            'password' => 'test123',
+        $this->call([
+            UserSeeder::class,
+            CategorySeeder::class,
         ]);
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,10 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\MembersController;
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\ProductsController;
+use App\Http\Controllers\ProductCategoryController;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,15 +17,31 @@ use App\Http\Controllers\ProfileController;
 |
 */
 
-Route::get('/', fn() => redirect()->route('/login'));
+Route::get('/', fn () => redirect()->route('login'));
 
-Route::get('/admin/dashboard', function(){
-    return view('layouts.admin-main');
+Route::get('/admin/dashboard', function () {
+    return view('admin.dashboard');
+})->middleware(['auth', 'verified'])->name('admin.dashboard');
+
+
+Route::group(['middleware' => 'auth'], function () {
+    Route::get('/category/data', [ProductCategoryController::class, 'data'])->name('category.data');
+    Route::resource('/category', ProductCategoryController::class);
+
+    Route::get('/products/data', [ProductsController::class, 'data'])->name('products.data');
+    Route::post('/products/delete-selected', [ProductsController::class, 'deleteSelected'])->name('products.delete_selected');
+    Route::post('/products/cetak-barcode', [ProductsController::class, 'cetakBarcode'])->name('products.cetak_barcode');
+    Route::resource('/products', ProductsController::class);
+
+    Route::get('/member/data', [MembersController::class, 'data'])->name('member.data');
+    Route::post('/member/cetak-member', [MembersController::class, 'cetakMember'])->name('member.cetak_member');
+    Route::resource('/member', MembersController::class);
 });
+
 
 Route::prefix('cashier')->group(function () {
     // Route::get('/', fn()=> redirect()->route('/dashboard'));
-    Route::get('/dashboard', fn()=> view('cashier.dashboard'))->middleware(['auth', 'verified'])->name('cashier.dashboard');
+    Route::get('/dashboard', fn () => view('cashier.dashboard'))->middleware(['auth', 'verified'])->name('cashier.dashboard');
     // Route::get('/sales', [CashierController::class, 'sales'])->name('cashier.sales');
     // Route::get('/transactions', [CashierController::class, 'transactions'])->name('cashier.transactions');
 });
@@ -34,4 +53,4 @@ Route::middleware('auth')->group(function () {
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
-require __DIR__.'/auth.php';
+require __DIR__ . '/auth.php';
