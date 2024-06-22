d9060a56632fa4aeda64f3dbb867eaffa5ef4132
finniaaa <144453150+finniaaa@users.noreply.github.com>
2024-06-22 22:46:54 +0700

    setting page

---
+++ app/Models/Purchases.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Models;
+
+use App\Models\Supplier;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+
+class Purchases extends Model
+{
+    use HasFactory;
+
+    protected $table = 'purchases';
+    protected $primaryKey = 'purchase_id';
+    protected $guarded = [];
+
+    public function supplier()
+    {
+        return $this->belongsTo(Supplier::class, 'supplier_id', 'supplier_id');
+    }
+}
---
+++ database/seeders/CategorySeeder.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+
+class CategorySeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        // Example data
+        $categories = [
+            [
+                'category_name' => 'Electronics',
+                'created_at' => now(),
+                'updated_at' => now(),
+            ],
+            [
+                'category_name' => 'Clothing',
+                'created_at' => now(),
+                'updated_at' => now(),
+            ],
+            [
+                'category_name' => 'Home Appliances',
+                'created_at' => now(),
+                'updated_at' => now(),
+            ],
+            [
+                'category_name' => 'Books',
+                'created_at' => now(),
+                'updated_at' => now(),
+            ],
+        ];
+
+        // Insert data into the database
+        DB::table('product_category')->insert($categories);
+    }
+}
