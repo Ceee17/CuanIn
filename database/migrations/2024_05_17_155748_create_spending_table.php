85d0f817d200a147af34c90f53a2fb4ac13a63dc
finniaaa <144453150+finniaaa@users.noreply.github.com>
2024-06-22 20:35:20 +0700

    membuat crud spending

---
+++ database/migrations/2024_05_17_155748_create_spending_table.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('spending', function (Blueprint $table) {
+            $table->increments('spending_id');
+            $table->text('description');
+            $table->integer('nominal');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('spending');
+    }
+};
